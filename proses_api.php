08d967c0d27629fdd9008029ebea9ef5a2aee57d
rowan6999 <71204716+rowan6999@users.noreply.github.com>
2020-12-10 10:01:16 +0700

    3

--- proses_api.php
+++ proses_api.php
@@ -1,21 +1,16 @@
-$konfir_password = $postison['konfir_password'];
-$agree $postjson['agree'];
-$created= $todayDate.";".$todayMonth.";".$todayYear;
-if($name == "}{
-$result = json_encode(array('success'=> false, 'msg' => "Nama tidak boleh kosong!'));
+Else{
+$password = md5($postjson['password']);
+$select = mysqli _query($mysqli, "SELECT * FROM 'user’ WHERE 'email' = ‘$postjsonjemail’");
+$row= mysqli_num_rows($select);
+if($row>= 1){
+$result = json_encode(array('success' => false, 'msg' => 'Email yang dimasukkan sudah terdaftar!'));
 }
-else if($email ==”"){
-$result = json_encode(array('success' => false, 'msg' => "Email tidak boleh kosong!"));
+else{
+$insert = mysqli_query($mysqli, "INSERT INTO 'user’ VALUES (null, '$postjson[namel', '$postjson[email]','$password', '$created','-‘, '-‘', '-‘)");
+if($insert){
+$result = json_encode(array('success' => true, 'msg' => "Sukses Mendaftakan Akun. Login melalui Menu yang tersedia.’));
+Else{
+$result = json_encode(array('success' => false, 'msg' => 'Gagal Melakukan Pendaftaran! Periksa formulir dan coba lagi.’));
 }
-else if($pass == "”){
-$result json_encode(array('success' => false, 'msg' => "Kata Sandi tidak boleh kosong!"));
 }
-else if($konfir_password=="”){
-$result json_encode(array('success' => false, 'msg' => "Harap Konfirmasi Kata Sandi!"));
-}
-else if($pass != $konfir_password){
-$result json_encode(array('success'=> false, 'msg' => "Kata Sandi Tidak Sama!"));
-}
-else if($agree != 1){
-$result json_encode(array('success' => false, 'msg' => "Anda harus menyetujui Syarat& Ketentuan"));
 }
